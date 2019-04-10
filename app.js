59642600dfc4aaa9bbbb1c4b6eecd12dc00e8fc1
Leilani Sumi <m.leilani.sumi@gmail.com>
2019-04-10 17:03:39 -0600

    moving snake

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ let snake = [
   {x: 110, y: 150},
 ];
 
+let dx = 10;
+let dy = 0;
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+function moveSnake () {
+  const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+        snake.unshift(head);
+        snake.pop();
+}
+
+function drawSnake () {
+  snake.forEach(drawSnakePart);
+}
+
+function drawSnakePart (snakePart) {
+  ctx.fillStyle = "#ffc7ad";
+  ctx.strokestyle = "#ffc7ad";
+}
+
+ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+
+ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+
 function draw() {
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
